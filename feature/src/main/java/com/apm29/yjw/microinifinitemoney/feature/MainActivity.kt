6cb1ab279a67fdb2c230ec7596926a91cceb91cc
apm29_win10 <yjw999wow@163.com>
2018-11-19 23:23:58 +0800

    notifications

--- feature/src/main/java/com/apm29/yjw/microinifinitemoney/feature/MainActivity.kt
+++ feature/src/main/java/com/apm29/yjw/microinifinitemoney/feature/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.apm29.yjw.microinifinitemoney.feature
 
-import android.app.Notification
+import android.app.*
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
@@ -14,6 +16,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Intent
+import android.support.v4.app.NotificationManagerCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,27 +31,58 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            createNotificationChannel()
+            createNotification()
         }
 
         // Example of a call to a native method
         sample_text.text = stringFromJNI()
 
 
-        GlobalScope.launch (Dispatchers.Main){
+        GlobalScope.launch(Dispatchers.Main) {
             delay(10000)
             sample_text.text = "I have changed after 10 seconds"
         }
 
-        val builder = Notification.Builder(this)
-            .setContentTitle("this is title")
-            .setSubText("subtext bla bla...")
-        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
-            builder.setChannelId("chnl_id_mim")
+    }
+
+    private fun createNotification() {
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
-        val notification = builder
-            .build()
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
+        val mBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(3, mBuilder.build())
+        }
+    }
 
+    val CHANNEL_ID = "1212"
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel_name_mim"
+            val descriptionText = "channel_desc_mim"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
